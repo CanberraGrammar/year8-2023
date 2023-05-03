2d6a6fe68744992866aa59da09a879107a1f3036
CGS-Alexander-Phillips <37096498+CGS-Alexander-Phillips@users.noreply.github.com>
2023-05-03 12:50:17 +1000

    Added t2w02 StarterCode

---
+++ Term 2/t2w02-StarterCode.py
@@ -0,0 +1,18 @@
+from machine import *
+import time
+
+# trigger = Pin(0, Pin.OUT)
+# echo = Pin(1, Pin.IN)
+# 
+# def measureUltrasonic():
+#     trigger.high()
+#     time.sleep_us(10)
+#     trigger.low()
+#     while echo.value() == 0:
+#         startTime = time.ticks_us()
+#     while echo.value() == 1:
+#         finishTime = time.ticks_us()
+#     timepassed = finishTime - startTime
+#     distance = (timepassed / 2) * 0.0343
+#     return distance
+
