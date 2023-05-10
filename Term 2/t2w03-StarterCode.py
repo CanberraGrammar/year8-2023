c9dc268596ea217ee770fe95abb6d5c87d0a4da1
CGS-Alexander-Phillips <37096498+CGS-Alexander-Phillips@users.noreply.github.com>
2023-05-10 13:16:06 +1000

    Added t2w03-StarterCode

---
+++ Term 2/t2w03-StarterCode.py
@@ -0,0 +1,24 @@
+from machine import *
+import time
+
+def getFrequency(note):
+    notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+    
+    if len(note) == 3:
+        octave = int(note[2])
+    else:
+        octave = int(note[1])
+
+    keyNumber = int(notes.index(note[:-1]))
+
+    if keyNumber < 3:
+        keyNumber = keyNumber + 12 + ((octave - 1) * 12) + 1
+    else:
+        keyNumber = keyNumber + (octave - 1) * 12 + 1
+
+    return 440 * (2 ** ((keyNumber - 49) / 12))
+
+Buzz = PWM(Pin(11))
+
+Buzz.duty_u16(int(0.5 * 65536))
+Buzz.freq(440)
