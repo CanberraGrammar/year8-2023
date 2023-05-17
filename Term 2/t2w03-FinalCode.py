5f607cb66fefa4352acc279f5660458c5d6ed9d3
CGS-Alexander-Phillips <37096498+CGS-Alexander-Phillips@users.noreply.github.com>
2023-05-17 12:50:03 +1000

    Fixed t2w03 final code

--- Term 2/t2w03-FinalCode.py
+++ Term 2/t2w03-FinalCode.py
@@ -1,22 +1,35 @@
 from machine import *
 import time
 
-Buzz = PWM(Pin(11))
+def getFrequency(note):
+    notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+    
+    if len(note) == 3:
+        octave = int(note[2])
+    else:
+        octave = int(note[1])
 
-def playTune(tune, tempo):
-    Buzz.duty_u16(int(0.5 * 65536))
+    keyNumber = int(notes.index(note[:-1]))
+
+    if keyNumber < 3:
+        keyNumber = keyNumber + 12 + ((octave - 1) * 12) + 1
+    else:
+        keyNumber = keyNumber + (octave - 1) * 12 + 1
+
+    return int(440 * (2 ** ((keyNumber - 49) / 12)))
+
+def playTune(tune,tempo,buzz):
     for note in tune:
         key, length = note
         if key == "R":
-            Buzz.duty_u16(0)
-            time.sleep(length*60/tempo)
-            Buzz.duty_u16(int(0.5 * 65536))
+            buzz.duty_u16(0)
         else:
-            Buzz.freq(getFrequency(key))
-            time.sleep(length*60/tempo)
-    Buzz.duty_u16(0)
+            buzz.duty_u16(int(0.5 * 65536))
+            freq = getFrequency(key)
+            buzz.freq(freq)
+        time.sleep(60/tempo*length)
+    buzz.duty_u16(0)
 
-tune = [("C4",1),("D4",0.5),("E4",0.5),("R",1),("F4",0.5),("G4",0.5)]
-tempo = 60
+Buzz = PWM(Pin(11))
 
-playTune(tune, tempo)
+playTune([("C4",1),("D4",0.5),("E4",0.5),("R",1),("F4",0.5),("G4",0.5),("R",1),("A4",1)],120,Buzz)
